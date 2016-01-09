48a4464f0b881d900b4ff275a720216affde3672
mbyrdziak <mbyrdziak@gmail.com>
2016-01-09 18:52:03 +0100

    initial commit

---
+++ src/Card.php
@@ -0,0 +1,73 @@
+<?php
+class Card {
+    public function checkDeadline($deadline) {
+        $deadlineDate = $deadline;
+        $today = new DateTime ();
+        $warningText = null;
+        if ($deadline == "0000-00-00" || $deadline == null)
+            $deadlineDate = "-";
+        if ($deadlineDate != "-") {
+            $diff = $today->diff(new DateTime($deadlineDate));
+            $diff_value = (int) $diff->format('%R%a');
+            if ($diff_value == 0)
+                $warningText = "Warning: the deadline date is $deadlineDate";
+            else if ($diff_value < 0)
+                $warningText = "Warning: the deadline date was $deadlineDate";
+            else
+                $warningText = null;
+        }
+        return $warningText;
+    }
+    
+    public function getStoryPointsValue($value, $boardType, $sprintId) {
+        $content = '';
+        $cardFamily = $this->getCardFamily($value['id'], 1);
+        $stateData = $this->getStateData($value ['states_id'], false);
+        $sumRSP = 0;
+        $sumESP = 0;
+        $msgText = 'all';
+
+        if ($cardFamily) {
+            if ($stateData ['state_type'] == 'inbox' || $boardType == 'kanban') {
+                foreach ($cardFamily as $val) {
+                    $sumRSP += $val ['reported_sp'];
+                    $sumESP += $val ['estimated_sp'];
+                }
+            } else {
+                $msgText = 'sprint';
+                if (isset($sprintId))
+                    foreach ($cardFamily as $val) {
+                        if ($sprintId == $val ['sprint_id']) {
+                            $sumRSP += $val ['reported_sp'];
+                            $sumESP += $val ['estimated_sp'];
+                        }
+                    }
+            }
+        }
+
+        if ($value != null)
+            if ($value['estimated_sp'] > 0) {
+                $content.= $value['reported_sp'] . '/' . $value['estimated_sp'];
+            } else if ($sumESP > 0 && $cardFamily) {
+                $content.= '<span class="familyERSP' . $value['id'] . ' estimatedreported_values" title="Work progress on ' . $msgText . ' children tasks">(<span class="estimatedReportedSp">' . $sumRSP . '/' . $sumESP . '</span>)</span>';
+            }
+
+        return $content;
+    }
+    
+    private function getCardFamily($id, $unknown) {
+        return array(
+            array(
+                'reported_sp' => 10,
+                'estimated_sp' => 10,
+            )
+        );
+    }
+
+    private function getStateData($id, $unknown) {
+        return array(
+            'state_type' => 'inbox',
+        );
+    }
+
+}
---
+++ test/src/CardTest.php
@@ -0,0 +1,6 @@
+<?php
+class CardTest extends PHPUnit_Framework_TestCase {
+    public function testNothing() {
+        $this->assertTrue(false);
+    }
+}
