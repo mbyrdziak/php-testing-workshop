20a0d422dcf49d989000c617245b4aa13d953e16
Jakub Cichy <jcic@ericpol.com>
2016-01-15 16:00:42 +0100

    mocki

--- test/src/Integration/AddOrderServiceStubTest.php
+++ test/src/Integration/AddOrderServiceStubTest.php
@@ -9,6 +9,7 @@
 namespace Test\Integration;
 use Workshop\Integration\AddOrderService;
 use Workshop\Integration\OrderRepository;
+use \Workshop\Integration\Order;
 /**
  * Description of AddOrderServiceStubTest
  *
@@ -36,7 +37,8 @@ class AddOrderServiceStubTest extends \PHPUnit_Framework_TestCase{
     
     /**
      * @test
-     * 
+     * TestDoubles - imitacja 
+     * STUB / MOCK - stub - pisanie klasy(sprawdza przeplyw, sciezke)/ mock sam generuje klase(sprawdza zachowanie)
      */
     public function executeShouldAddOrder() {
         //given
@@ -49,5 +51,56 @@ class AddOrderServiceStubTest extends \PHPUnit_Framework_TestCase{
         $this->assertEquals("1",$id);
         $this->assertEquals("127.0",$orderElement->getPickupAddress());
         $this->assertEquals("0.1",$orderElement->getShippingAddress());
+    }
+    
+    /**
+     * @test
+     * MOCK 
+     */
+    public function executeShouldAddOrderMock() {
+        //given
+        $orderRepoMock = $this->getMockBuilder("Workshop\Integration\OrderRepository")
+                ->disableOriginalConstructor()->getMock();
+        $service = new AddOrderService($orderRepoMock);
+        //when
+        $order = new Order(null,"pickup","shipping",0,0);
+        $orderRepoMock->expects($this->once())->method('persist')
+                ->with($this->equalTo($order));
+        //then
+        $service->execute("pickup","shipping");
+    }
+    
+    /**
+     * @test
+     * MOCK 
+     */
+    public function executePersistReturnValueMock() {
+        //given
+        $orderRepoMock = $this->getMockBuilder("Workshop\Integration\OrderRepository")
+                ->disableOriginalConstructor()->getMock();
+        $orderRepoMock->method("persist")->will($this->returnValue(1));
+        $service = new AddOrderService($orderRepoMock);
+        //when
+        $orderId = $service->execute("addr","ship");
+        //then
+        $this->assertEquals(1, $orderId);
+    }
+    
+    /**
+     * @test
+     * MOCK 
+     */
+    public function executeShouldAddOrderMock2() {
+        //given
+        $orderRepoMock = $this->getMockBuilder("Workshop\Integration\OrderRepository")
+                ->disableOriginalConstructor()->getMock();
+        $service = new AddOrderService($orderRepoMock);
+        //when
+        $order = new Order(null,"pickup","shipping",0,0);
+//        $orderRepoMock->
+        $orderRepoMock->expects($this->once())->method('persist')
+                ->with($this->equalTo($order));
+        //then
+        $service->execute("pickup","shipping");
     }
 }
